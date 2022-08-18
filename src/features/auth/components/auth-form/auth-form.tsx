d8cc4151cbaa8e319dc4c03cd9d04f521e6b8eda
Baciu Florin-George <florin.baciu@assist.ro>
2022-08-18 17:36:57 +0300

    +

--- src/features/auth/components/auth-form/auth-form.tsx
+++ src/features/auth/components/auth-form/auth-form.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { InputText } from 'primereact/inputtext'
 
+import styles from './auth-form.module.scss'
 interface Props {
   formValues: { email: string; password: string }
   setFormValues: (value: string, field: 'email' | 'password') => void
@@ -10,16 +11,16 @@ export const AuthForm = ({ formValues, setFormValues }: Props) => {
   const [isPasswordHidden, setIsPasswordHidden] = useState<boolean>(false)
 
   return (
-    <>
+    <div className={styles.authForm}>
       <h5>Email</h5>
       <InputText value={formValues.email} onChange={(e) => setFormValues(e.target.value, 'email')} />
       <h5>Password</h5>
-      <i className='pi pi-spin pi-spinner' onClick={() => setIsPasswordHidden((state) => !state)} />
+      <p onClick={() => setIsPasswordHidden((state) => !state)}>eye</p>
       <InputText
         type={isPasswordHidden ? 'password' : 'text'}
         value={formValues.password}
         onChange={(e) => setFormValues(e.target.value, 'password')}
       />
-    </>
+    </div>
   )
 }
