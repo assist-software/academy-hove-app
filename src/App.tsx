3b1ae90637670503ef68f9b2454db99542e565c1
Baciu Florin-George <florin.baciu@assist.ro>
2022-08-22 16:23:37 +0300

    +

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,15 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import 'primereact/resources/primereact.min.css' //core css
 import 'primeicons/primeicons.css'
 import './App.css'
+import { PAGES_PATHS } from './common/constants/constants'
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<>HI</>}></Route>
-          <Route path='/auth' element={<AuthPage />}></Route>
+          <Route path={PAGES_PATHS.HOME} element={<>HI</>}></Route>
+          <Route path={PAGES_PATHS.LOGIN} element={<AuthPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/common/constants/constants.ts
@@ -0,0 +1,5 @@
+export const PAGES_PATHS = {
+  LOGIN: '/login',
+  SIGN_UP: '/signup',
+  HOME: '/',
+}
