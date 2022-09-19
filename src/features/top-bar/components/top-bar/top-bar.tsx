0cdd27af13ce396e2e8906de721be28c2616028a
Baciu Florin-George <florin.baciu@assist.ro>
2022-09-19 17:23:43 +0300

    moved the searchbar and category selector to the showroom feature

--- src/features/top-bar/components/top-bar/top-bar.tsx
+++ src/features/top-bar/components/top-bar/top-bar.tsx
@@ -1,40 +1,19 @@
-import { Dropdown, DropdownChangeParams } from 'primereact/dropdown'
-import { Link, useNavigate } from 'react-router-dom'
-import { InputText } from 'primereact/inputtext'
-import classNames from 'classnames'
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import { AUTH_PAGES, PAGES_PATHS } from 'common/constants/constants'
 import { useStore } from 'store/store'
 
-import { DROPDOWN_PLACEHOLDER } from 'features/top-bar/constants/top-bar-constants'
-import { ReactComponent as FavIcon } from '../../assets/favourites-icon.svg'
+import { SearchBar } from 'features/showroom/components/showroom-search-bar/showroom-search-bar'
+
 import { ReactComponent as AssistLogo } from 'common/assets/logo-assist.svg'
+import { ReactComponent as FavIcon } from '../../assets/favourites-icon.svg'
 import { ReactComponent as UserIcon } from '../../assets/user-icon.svg'
 
 import style from './top-bar.module.scss'
 
 export const TopBar = () => {
-  const [selectCategory, setSelectedCategory] = useState(null)
-
   const { authStore } = useStore()
   const { userName } = authStore
-
-  const navigate = useNavigate()
-
-  const categories = [{ name: 'Category 1' }, { name: 'Category 2' }, { name: 'Category 3' }]
-
-  const onCityChange = (e: DropdownChangeParams) => {
-    setSelectedCategory(e.value)
-  }
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const textBox = event.target as HTMLInputElement
-
-    if (event.key === 'Enter') {
-      navigate(`${PAGES_PATHS.SEARCH}/${textBox.value}`)
-    }
-  }
 
   if (AUTH_PAGES.includes(document.location.pathname)) return <></>
 
@@ -47,24 +26,12 @@ export const TopBar = () => {
           </Link>
         </div>
         <div className={style.topBarSearch}>
-          <div className='p-inputgroup'>
-            <span className={classNames('p-inputgroup-addon', style.topBarInputCategoryHolder)}>
-              <Dropdown
-                className={style.topBarInputCategory}
-                value={selectCategory}
-                options={categories}
-                onChange={onCityChange}
-                optionLabel='name'
-                placeholder={DROPDOWN_PLACEHOLDER}
-              />
-            </span>
-            <InputText onKeyDown={handleKeyDown} />
-          </div>
+          <SearchBar />
         </div>
         <div className={style.topBarElement}>
           <Link to={PAGES_PATHS.FAVORITES} className={style.topBarLink}>
             <FavIcon className={style.topBarLinkIcon} />
-            <span className={style.topBarLinkText}>Favourites</span>
+            <span className={style.topBarLinkText}>Favorites</span>
           </Link>
         </div>
         <div className={style.topBarElement}>
