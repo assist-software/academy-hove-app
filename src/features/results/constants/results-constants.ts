afe077e73390e2cec91458c623375e4d4756dcd6
AdrianCostescu <71406940+AdrianCostescu@users.noreply.github.com>
2022-10-03 21:28:09 +0300

    results-photo

---
+++ src/features/results/constants/results-constants.ts
@@ -0,0 +1,23 @@
+export const PROPRETY_ALT_IMG = {
+  IMAGE_HOME: 'Home Photo',
+  SHOW: 'Show all photo',
+  AVATAR: 'User Photo',
+  SHARE: 'Share Icon',
+  FAVORITE: 'Favorite Icon',
+}
+
+export const PROPRETY_BUTTON = {
+  ALL_IMG: 'Show all photos',
+  PURCHASE: 'Purchase',
+  DESCRIPTION: 'Description',
+  SHARE: 'Share',
+  SEND: 'Send',
+}
+
+export const PROPRETY_LABELS = {
+  MESSAGES_LABELS: 'Message the seller',
+}
+
+export const PROPRETY_PLACEHOLDER = {
+  TEXTAREA: 'Messages..',
+}
