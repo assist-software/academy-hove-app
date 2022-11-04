e35d49b03c76aec92ca8c9be835c60c80eec04b4
AdrianCostescu <71406940+AdrianCostescu@users.noreply.github.com>
2022-11-04 12:14:02 +0200

    update

--- src/features/add-new/components/add-new-description/add-new-description.tsx
+++ src/features/add-new/components/add-new-description/add-new-description.tsx
@@ -1,21 +1,21 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 
 import styles from './add-new-description.module.scss'
 
 interface IProps {
   title: string
   text: string
-  children?: any
+  children?: ReactNode
 }
 
 export const AddNewDescription: FC<IProps> = ({ title, text, children }) => {
   return (
-    <div className={styles.description}>
-      <div className={styles.descriptionDetails}>
+    <div className={styles.addNewDescription}>
+      <div className={styles.addNewDescriptionDetails}>
         <h2>{title}</h2>
         <p>{text}</p>
       </div>
-      <div className={styles.descriptionChildren}>{children}</div>
+      <div className={styles.addNewDescriptionChildren}>{children}</div>
     </div>
   )
 }
